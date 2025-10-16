1e08af1e071886be0673bf265332c1acec7b3f17
Ekin Ilter Varli <ekinnos@tutanota.com>
2025-10-16 20:45:09 +0300

    frontend update

--- licensing/licensing/settings.py
+++ licensing/licensing/settings.py
@@ -70,6 +70,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
